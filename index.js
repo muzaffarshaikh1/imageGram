84a9bba1d36b704e0970ad81b3178f8a9355cc50
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-10-16 22:03:19 +0530

    uploaded file on cloudinary

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import express from 'express';
 import connectDb from './config/dbConfig.js';
+
+import cloudinary from './utils/cloudinary.js';
+import upload from './middlewares/multer.js';
+import fs from 'fs';
+
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('Home');
+    res.send('<form action="/upload-file" method="post" enctype="multipart/form-data"><input type="file" name="file" /><button type="submit">upload</button></form>');
+});
+
+app.post('/upload-file',upload.single('file'),async function (req, res) {
+  try {
+
+    // Upload to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'uploads', // optional folder name
+    });
+
+    console.log("Cloudinary upload success:", result);
+
+    fs.unlinkSync(req.file.path);
+
+    res.json({
+      message: 'File uploaded successfully!',
+      cloudinary_url: result.secure_url,
+    });
+  } catch (error) {
+    console.error("Cloudinary upload error:", error);
+    res.status(500).json({ error: 'Upload failed!' });
+  }
+  
 });
 
 app.listen(3000,()=>{
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
 import dotenv from'dotenv';
 dotenv.config();
 
@@ -10,21 +9,4 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadImageToCloud = async (localFilePath) => {
-    try {
-        if(!localFilePath) return null;
-
-        const response = await cloudinary.uploader.upload(localFilePath,{resource_type:'auto'});
-
-        if(response){
-         console.log("file uploaded successfully");   
-        }
-        return response;
-    } catch (error) {
-        console.log("error:",error);
-        fs.unlinkSync(localFilePath);
-        return null;
-    }
-}
-
-export default uploadImageToCloud;
+export default cloudinary;
